d0b82d3fb26719ec6edeac62a3a6204adfc0b2cd
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2020-07-22 21:42:48 -0400

    Broken dummy bindings

--- app/src/main/java/com/masterwok/shrimplesearch/MainActivity.kt
+++ app/src/main/java/com/masterwok/shrimplesearch/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.masterwok.shrimplesearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import com.masterwok.jackett.JackettHarness
+import com.masterwok.xamarininterface.contracts.ICardigannDefinitionRepository
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val jackettHarness = JackettHarness(object : ICardigannDefinitionRepository {
+
+            override fun getDefinitions(): List<String> {
+                return emptyList()
+            }
+
+            override fun getIndexerCount(): Int {
+                return 0
+            }
+        })
+
+        var isInitialized = jackettHarness.initialize()
     }
 }
--- xamarininterface/src/main/java/com/masterwok/xamarininterface/contracts/IJackettHarness.kt
+++ xamarininterface/src/main/java/com/masterwok/xamarininterface/contracts/IJackettHarness.kt
@@ -1,4 +1,9 @@
 package com.masterwok.xamarininterface.contracts
 
 interface IJackettHarness {
+
+    val isInitialized: Boolean
+
+    fun initialize()
+
 }
