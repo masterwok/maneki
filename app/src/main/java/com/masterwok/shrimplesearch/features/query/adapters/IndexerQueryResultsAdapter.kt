66e57cef1e324520ecc15ed7e54579ff4f970075
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2020-08-08 12:24:14 -0400

    Indexer query result item layout

--- app/src/main/java/com/masterwok/shrimplesearch/features/query/adapters/IndexerQueryResultsAdapter.kt
+++ app/src/main/java/com/masterwok/shrimplesearch/features/query/adapters/IndexerQueryResultsAdapter.kt
@@ -6,8 +6,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.masterwok.shrimplesearch.R
 import com.masterwok.shrimplesearch.common.contracts.Configurable
+import com.masterwok.shrimplesearch.common.extensions.getCurrentLocale
 import com.masterwok.shrimplesearch.common.extensions.getLocaleNumberFormat
 import com.masterwok.xamarininterface.models.QueryResultItem
+import kotlinx.android.synthetic.main.view_indexer_query_result_item.view.*
+import java.text.DateFormat
 
 class IndexerQueryResultsAdapter(
     private val onQueryResultClicked: (QueryResultItem) -> Unit
@@ -23,7 +26,7 @@ class IndexerQueryResultsAdapter(
         LayoutInflater
             .from(parent.context)
             .inflate(
-                R.layout.view_query_result_item
+                R.layout.view_indexer_query_result_item
                 , parent
                 , false
             )
@@ -50,13 +53,42 @@ class IndexerQueryResultsAdapter(
         , Configurable<QueryResultItem> {
 
         override fun configure(model: QueryResultItem) {
-            val numberFormat = itemView.context.getLocaleNumberFormat()
+            val context = itemView.context
+            val statInfo = model.statInfo
+            val stringNotAvailable = context.getString(R.string.stat_info_not_available)
+            val numberFormat = context.getLocaleNumberFormat()
+            val dateFormat = DateFormat.getDateInstance(
+                DateFormat.SHORT
+                , context.getCurrentLocale()
+            )
 
-//            itemView.textViewIndexerName.text = model.indexer.displayName
-//            itemView.textViewMagnetCount.text = numberFormat.format(model.magnetCount)
-//            itemView.textViewLinkCount.text = numberFormat.format(model.linkCount)
+            itemView.textViewTitle.text = model.title
 
-//            itemView.setOnClickListener { onQueryResultClicked(model) }
+            itemView.textViewSeeders.text = statInfo
+                .seeders
+                ?.let(numberFormat::format)
+                ?: stringNotAvailable
+
+            itemView.textViewLeechers.text = statInfo
+                .peers
+                ?.let(numberFormat::format)
+                ?: stringNotAvailable
+
+            itemView.textViewFileCount.text = statInfo
+                .files
+                ?.let(numberFormat::format)
+                ?: stringNotAvailable
+
+            itemView.textViewPublishedOn.text = statInfo
+                .publishedOn
+                ?.let(dateFormat::format)
+                ?: stringNotAvailable
+
+            itemView.textViewSize.text = statInfo
+                .size
+                ?.toString()
+                ?: stringNotAvailable
+
         }
     }
 }
